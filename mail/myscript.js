8b677a9506cb74423ae6df2e7aa4e9973dfcccb4
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-04-21 17:27:15 +0200

    sistemazione if

--- mail/myscript.js
+++ mail/myscript.js
@@ -4,11 +4,17 @@
 var email_utente = prompt('Inserisci la tua mail');
 console.log('email_utente');
 //creazione lista delle email
-var lista_mail = ['mariorossi@gmail.com', 'pincopallo@gmail.com', 'paperino@gmail.com']
-console.log(lista_mail[0]);
-//controllo che sia in lista per accedere
-if (email_utente == lista_mail) {
-    console.log('puoi accedere');
-}else{
-    console.log('non puoi accedere');
+var lista_mail = ['mariorossi@gmail.com', 'pincopallo@gmail.com', 'paperino@gmail.com','pape@gmail.com','pano@gmail.com'];
+var risultato = false;
+for (var i = 0; i < lista_mail.length; i++) {
+    if (email_utente == lista_mail[i]) {
+        risultato = true;
+    }
 }
+ if (risultato == true) {
+     console.log('puoi accedere');
+     document.getElementById('text').innerHTML = 'Puoi accedere';
+ } else {
+     console.log('non puoi accedere');
+     document.getElementById('text').innerHTML = 'Non puoi accedere';
+ }
