403a7641978e24f873d836852abfb3ff7d09163b
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-04-21 18:14:04 +0200

    prova if

--- dadi/myscript.js
+++ dadi/myscript.js
@@ -1,7 +1,15 @@
 //Gioco dei dadi, chi fa di più vince
 //Chiedere all'utente di scrivere un numero
-var numero_utente = prompt('Scrivi un numero da 1 a 6')
+var numero_utente = prompt('Scrivi un numero da 1 a 6');
 console.log(numero_utente);
 //numero generato dal computer
-var numero_generato = Math.floor(Math.random() * 6)
+var numero_generato = Math.floor(Math.random() * 6);
 console.log(numero_generato);
+
+if (numero_utente > numero generato) {
+    console.log('vincitore utente');
+} else if (numero utente == numero_generato){
+    console.log('pareggio');
+} else {
+    console.log('vincitore computer');
+}
